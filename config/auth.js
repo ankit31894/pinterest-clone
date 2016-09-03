2afb5dbc7ae7ed259b2e8f85084dedf947862ccd
Ankit <ankit31894@gmail.com>
2016-09-03 21:29:04 +0530

    Changed callback url for Twitter Authentication

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
  	'twitterAuth' : {
         'consumerKey'       : 'YUiBjHx4CJ2fbkOq6Z9hyOMkK',
         'consumerSecret'    : 'o9EExZrSWdODRTvEeOrPRGkdoLijWXiZSZRazYdcxpJwEiED5I',
-        'callbackURL'       : 'http://127.0.0.1:8080/login/twitter/callback'
+        'callbackURL'       : 'https://ankit31894-pinterest.herokuapp.com/login/twitter/callback'
     }
 };
